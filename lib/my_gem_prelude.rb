f7b7d6ea1a8ec0bd79bf5f2b2cd1e43d0c6e822c
fistfvck <fistfvck@gmail.com>
2010-06-22 15:12:15 +0900

    bug?

--- lib/my_gem_prelude.rb
+++ lib/my_gem_prelude.rb
@@ -278,7 +278,7 @@ if defined?(Gem) then
           require_paths.first.instance_variable_set(:@gem_prelude_index, true)
         end
         # gem directories must come after -I and ENV['RUBYLIB']
-        $:[$:.find{|e|e.instance_variable_defined?(:@gem_prelude_index)}||-1,0] = require_paths
+        $:[$:.find_index{|e|e.instance_variable_defined?(:@gem_prelude_index)}||-1,0] = require_paths
       end
 
       def const_missing(constant)
